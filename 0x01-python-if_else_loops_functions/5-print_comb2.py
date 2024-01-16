5c42bc9c28fb0e80f2f5d4bbfdca73b2b080dbb5
Suhayla344 <shylhk228@gmail.com>
2024-01-17 01:41:17 +0200

    python

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for x in range(10):
     for y in range(10):
-        print("{}{},".format(x,y), end=" ")
+        if x != 9 and y != 9:
+            print("{}{},".format(x,y), end=" ")
+print(99)
