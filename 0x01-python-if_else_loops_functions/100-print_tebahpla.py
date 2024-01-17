d49a874dff853610ac9c956224e26af0fbef880b
Suhayla344 <shylhk228@gmail.com>
2024-01-17 04:09:30 +0200

    pp

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for x in reversed(range(97, 123)):
-    counter = x
+for i in reversed(range(97, 123)):
+    counter = i
     if counter % 2 != 0:
         counter -= 32
     print("{}".format(chr(counter)), end="")
