dc4a7afc015386009b3b02eb2beea75cafe85a23
Suhayla344 <shylhk228@gmail.com>
2024-02-08 01:01:35 +0200

    soska

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+mport sys
+
+
+def safe_function(fct, *args):
+        try:
+            return fct(*args)
+        except Exception as err:
+            print("Exception: {}".format(err), file=sys.stderr)
+            return None
