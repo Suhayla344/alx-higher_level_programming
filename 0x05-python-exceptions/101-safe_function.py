725cf425401284a2dcbc54ab4b32a78beeb6bc64
Suhayla344 <shylhk228@gmail.com>
2024-02-08 01:51:04 +0200

    soska

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,10 @@
-mport sys
+#!/usr/bin/python3
+import sys
 
 
 def safe_function(fct, *args):
-        try:
-            return fct(*args)
-        except Exception as err:
-            print("Exception: {}".format(err), file=sys.stderr)
-            return None
+    try:
+        return fct(*args)
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
