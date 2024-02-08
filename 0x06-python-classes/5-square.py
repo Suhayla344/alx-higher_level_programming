509f321b418f156eba4f096997ab52d99ba16135
Suhayla344 <shylhk228@gmail.com>
2024-02-08 03:04:43 +0200

    soska

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -5,31 +5,24 @@
 
 class Square:
     """initialization of an instance with size attribute
+    with size attribute
     """
     def __init__(self, size):
         self.size = size
     """ size(self) - retrievess the private attribute size (gitter)"""
     @property
     def size(self):
-        """set the actual size of  the square"""
         return self.__size
+
         """def size(self, value): setter for
         private attribute size (setter) """
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
-            """Area: returns area of a square """
-        def area(self):
-            return self.__size * self.__size
-
-        def my_print(self):
-            """this prints the suare with # character"""
-            for i in range(0, self.__size):
-                [print("#", end="") for j in range(self.__size)]
-                print("")
-            if self.__size == 0:
-                print("")
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    """Area: returns area of a square """
+    def area(self):
+        return self.__size * self.__size
